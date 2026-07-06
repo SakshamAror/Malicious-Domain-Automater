fd7f2efd27ff532bb7c8cce09500ac7a8bc67377
Saksham Arora <80617896+SakshamAror@users.noreply.github.com>
2026-07-06 12:16:03 +0000

    Made python files and logical changes

---
+++ csv_parser.py
@@ -0,0 +1,16 @@
+import csv
+
+domain_col_name = "email ID"
+domain_list = []
+
+with open("domains.csv", "r") as f:
+  data = csv.DictReader(f)
+  for row in data:
+      for dom in row[domain_col_name].split(";"):
+        split = dom.strip().split("@")
+        if len(split) == 2 and split[1] not in domain_list:
+          domain_list.append(split[1])
+  
+  print(domain_list)
+    
+  print(f"Total domains to process: {len(domain_list)}")
---
+++ domain_checker.py
@@ -0,0 +1,36 @@
+from playwright.sync_api import sync_playwright
+import os
+
+domain_list = ["greenmountains.ae", "yahoo.com", "biorus.ae", "188[.]92[.]255[.]57", "pragmaticus[.]ru"]
+safe_text = "No security vendors flagged this domain as malicious"
+safe_rating = "0"
+suspicious_list = []
+
+with sync_playwright() as p:
+    browser = p.chromium.launch()
+    page = browser.new_page()
+
+    for dom in domain_list:
+        page.goto("https://www.virustotal.com/gui/search?query=" + dom)
+
+        eval_locator = page.locator("div > div.card-header.hstack.flex-wrap.justify-content-between.gap-2 > div.hstack.gap-2.fw-bold[class*='text-']")
+        rating_locator = page.locator("#positives")
+        eval_locator.wait_for(state="visible")
+        rating_locator.wait_for(state="visible")
+        text = eval_locator.text_content()
+        rating = rating_locator.text_content()
+        rating = rating.strip()
+
+        if rating != safe_rating and text != safe_text:
+            print(f"------------ Suspicious Domain: {dom}")
+            page.screenshot(path= os.path.join("screenshots", dom + ".png"))
+            suspicious_list.append(dom)
+        else:
+            print(f"--- Domain: {dom}")
+    
+        print(f"Evaluation: {text}")
+        print(f"Rating: {rating}/91")
+
+    browser.close()
+    print("Suspicious Domains: " + len(suspicious_list))
+    print(suspicious_list)
